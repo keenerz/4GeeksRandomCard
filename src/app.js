a6bb1dd98eaa21cf92509911ce07328d4742c74e
Keenan <kkan68@gmail.com>
2021-12-08 03:23:20 +0000

    mostly done

--- src/app.js
+++ src/app.js
@@ -6,6 +6,49 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  document.querySelector(".card").classList.add(randomSuitGen());
+  document.querySelector(".number").innerHTML = randomNumberGen();
+  setInterval(() => {
+    document.querySelector(".card").classList.remove("heart");
+    document.querySelector(".card").classList.remove("club");
+    document.querySelector(".card").classList.remove("diamond");
+    document.querySelector(".card").classList.remove("spade");
+    document.querySelector(".card").classList.add(randomSuitGen());
+    document.querySelector(".number").innerHTML = randomNumberGen();
+  }, 10000);
 };
+
+function randomNumberGen() {
+  let numbers = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+  let numberIndex = Math.floor(Math.random() * numbers.length);
+  return numbers[numberIndex];
+}
+
+function randomSuitGen() {
+  let suits = ["diamond", "spade", "heart", "club"];
+  let suitIndex = Math.floor(Math.random() * suits.length);
+  return suits[suitIndex];
+}
+
+document.querySelector("#newCard").addEventListener("click", function() {
+  document.querySelector(".card").classList.remove("heart");
+  document.querySelector(".card").classList.remove("club");
+  document.querySelector(".card").classList.remove("diamond");
+  document.querySelector(".card").classList.remove("spade");
+  document.querySelector(".card").classList.add(randomSuitGen());
+  document.querySelector(".number").innerHTML = randomNumberGen();
+});
